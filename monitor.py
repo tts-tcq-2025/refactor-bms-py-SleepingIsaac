50acbaf6dbb98a69f60f4ee626e0d776cfb00b1a
SleepingIsaac <102993454+SleepingIsaac@users.noreply.github.com>
2025-09-17 09:31:38 -0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,10 +8,16 @@ def is_spo2_ok(spo2):
     #Checks if the oxygen saturation (SpO2) is within the acceptable range
     return spo2 >= 90
 
-def warning_animation():
+def warning_animation(tempOK, pulseOK, spo20K):
     #Displays a warning animation
     for i in range(6):
         print('\r* ', end='')
+        if(tempOK):
+            print(" Temperature Warning ")
+        elif(pulseOK):
+            print(" Pulse Warning ")
+        elif(spo20K):
+            print(" Spo2 Warning ")
         sys.stdout.flush()
         sleep(1)
         print('\r *', end='')
@@ -20,16 +26,10 @@ def warning_animation():
 
 def vitals_ok(temperature, pulse_rate, spo2):
     #Checks if all vitals are are ok
-    if not is_temperature_ok(95, 102, temperature):
-        print('Temperature critical!')
-        warning_animation()
-        return False
-    elif not is_pulse_rate_ok(60, 100, pulse_rate):
-        print('Pulse Rate is out of range!')
-        warning_animation()
-        return False
-    elif not is_spo2_ok(spo2):
-        print('Oxygen Saturation out of range!')
-        warning_animation()
+    tempOK  = is_temperature_ok(95, 102, temperature)
+    pulseOK = is_pulse_rate_ok(60, 100, pulse_rate)
+    spo2OK  = s_spo2_ok(spo2)
+    if(tempOK or pulseOK or spo20K):
+        wrong_animation(tempOK, pulseOK, spo20K)
         return False
     return True
