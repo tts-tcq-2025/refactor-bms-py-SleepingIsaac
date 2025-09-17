8b38949c720c4161c36b450f635f1aebedf51432
SleepingIsaac <102993454+SleepingIsaac@users.noreply.github.com>
2025-09-17 09:20:09 -0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,13 +1,8 @@
 import sys
 from time import sleep
 
-def is_temperature_ok(temperature):
-    #Checks if the temperature is within the acceptable range
-    return 95 <= temperature <= 102
-
-def is_pulse_rate_ok(pulse_rate):
-    #Checks if the pulse rate is within the acceptable range
-    return 60 <= pulse_rate <= 100
+def is_between_range(min, max, value):
+    return min <= value <= max
 
 def is_spo2_ok(spo2):
     #Checks if the oxygen saturation (SpO2) is within the acceptable range
@@ -25,11 +20,11 @@ def warning_animation():
 
 def vitals_ok(temperature, pulse_rate, spo2):
     #Checks if all vitals are are ok
-    if not is_temperature_ok(temperature):
+    if not is_temperature_ok(95, 102, temperature):
         print('Temperature critical!')
         warning_animation()
         return False
-    elif not is_pulse_rate_ok(pulse_rate):
+    elif not is_pulse_rate_ok(60, 100, pulse_rate):
         print('Pulse Rate is out of range!')
         warning_animation()
         return False
@@ -37,5 +32,4 @@ def vitals_ok(temperature, pulse_rate, spo2):
         print('Oxygen Saturation out of range!')
         warning_animation()
         return False
-    print("Vitals are OK!")
     return True
