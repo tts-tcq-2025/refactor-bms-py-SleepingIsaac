461af193d1d61b031675ff3cad12d832ddb5bb93
SleepingIsaac <102993454+SleepingIsaac@users.noreply.github.com>
2025-09-17 08:48:33 -0600

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,44 @@
-
-from time import sleep
 import sys
+from time import sleep
 
+def is_temperature_ok(temperature):
+    """Checks if the temperature is within the acceptable range."""
+    return 95 <= temperature <= 102
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
+def is_pulse_rate_ok(pulse_rate):
+    """Checks if the pulse rate is within the acceptable range."""
+    return 60 <= pulse_rate <= 100
+
+def is_spo2_ok(spo2):
+    """Checks if the oxygen saturation (SpO2) is within the acceptable range."""
+    return spo2 >= 90
+
+def warning_animation():
+    """Displays a warning animation."""
     for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+
+def vitals_ok(temperature, pulse_rate, spo2):
+    """
+    Main function to check if all vitals are okay.
+    Combines I/O and logic checks.
+    """
+    if not is_temperature_ok(temperature):
+        print('Temperature critical!')
+        warning_animation()
+        return False
+    elif not is_pulse_rate_ok(pulse_rate):
+        print('Pulse Rate is out of range!')
+        warning_animation()
+        return False
+    elif not is_spo2_ok(spo2):
+        print('Oxygen Saturation out of range!')
+        warning_animation()
+        return False
+    print("Vitals are OK!")
+    return True
